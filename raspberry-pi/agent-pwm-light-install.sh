75e3c53f74ee35ce14e0fe2386ee738bb1bdff16
Olivier Watté <owatte@users.noreply.github.com>
2019-10-06 18:14:06 -0400

    Update agent-pwm-light-install.sh

--- raspberry-pi/agent-pwm-light-install.sh
+++ raspberry-pi/agent-pwm-light-install.sh
@@ -6,14 +6,10 @@
 # @License: GPLv3
 # @Copyright: IPEOS I-Solutions
 
-echo "Construction docker PWM"
-cd ~/src
-git clone https://github.com/sarfata/pi-blaster.git
-cd pi-blaster
-docker build -t pi-blaster .
 echo "Lancement docker PWM en tant que service"
 /usr/bin/docker run -itd --privileged --rm -v /dev:/dev pi-blaster
 (crontab -l ; echo "@reboot /usr/bin/docker run -itd --privileged --rm -v /dev:/dev pi-blaster &") 2>&1 | grep -v "no crontab" | sort | uniq | crontab -
+
 echo "Installation commandes RLIEH"
 sudo pip3 install rlieh-satlight rlieh_pwm
 sudo mkdir /var/log/rlieh/
